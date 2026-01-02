c9d6f44035e3df03c864e14413da76decf0d835d
Heli Gandhi <heligandhi4114@gmail.com>
2026-01-02 13:06:39 +0530

    Day 6 completed .

---
+++ PROJECT-6/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+function App() {
+  let mydata = [1,2,3,4,5,6,7,8,9,10]
+
+
+  
+
+
+  return ( 
+  <>
+  {/* practice */}
+    <h3>map function in react.</h3>
+    {mydata.map((value,index) => {
+      return(
+        <>
+        <li>{index+1}-{value}</li>
+        </>
+      )
+
+    }
+    )}
+
+
+{/* practice */}
+
+
+
+
+
+  </> );
+}
+
+export default App;
